af366e8a2a60ce60ec8a26a36e8098b184e77fd0
Boris Pazova <niklas@DESKTOP-NIQ7SCJ.localdomain>
2025-09-17 12:09:27 +0200

    add ignore migrations and ngrok settings

--- beauty_salon/settings/dev.py
+++ beauty_salon/settings/dev.py
@@ -9,6 +9,16 @@ SECRET_KEY = "django-insecure-95y(+ng_jcm(6+$&qziyg)h_00o9i$$o#8v9%-jm93^$(02!!y
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
 
+# Trusted origins for ngrok and local development
+CSRF_TRUSTED_ORIGINS = [
+    "https://test.statcats.net",
+    "https://beautyapp.ngrok.app",
+    "https://*.ngrok.app",
+    "https://*.ngrok.io",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
